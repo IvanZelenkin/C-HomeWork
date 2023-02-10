4fcf610f9dd881e8229199781bd323a445aa5d15
IvanZelenkin <vanya-200@yandex.ru>
2023-02-10 18:23:56 +0300

    Поиск максимального числа из 3 переменных

---
+++ HM_№2/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите третье число");
+int C = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+
+if (A > B)
+{
+    max = A;
+}
+else if (B > A)
+{
+    max = B;
+}
+if (max > C)
+{
+    System.Console.WriteLine("Максимальное число = " + max);
+}
+else if (C > max)
+{
+    max = C;
+    System.Console.WriteLine("Максимальное число = " + max);
+}
