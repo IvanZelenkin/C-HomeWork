f7ea67692c70bf3f2b7bd9cbb83f8ad80becc019
IvanZelenkin <vanya-200@yandex.ru>
2023-02-11 20:17:34 +0300

    Вывод четных чисел от 2 до n

---
+++ HW_№4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Запрос числа N
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Четные числа от 1 до N: ");
+
+// Цикл от 1 до N 
+for (int i = 1; i <= N; i++)
+{
+    // Если i четное, то вывести его на экран
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
