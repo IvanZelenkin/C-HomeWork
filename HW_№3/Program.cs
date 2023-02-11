aea3609b9106ac07e746b9844e4e2d675a36ff6a
IvanZelenkin <vanya-200@yandex.ru>
2023-02-11 20:06:55 +0300

    Проверка целого числа на четность/нч

---
+++ HW_№3/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine(n + " четное");
+}
+else
+{
+    Console.WriteLine(n + " нечетное");
+}
